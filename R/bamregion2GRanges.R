c441691a8fa0473689e598310e808ebe5c7e607b
daewoooo <daewoooo@gmail.com>
2017-11-13 16:10:23 +0100

    keepseqlevels bug

--- R/bamregion2GRanges.R
+++ R/bamregion2GRanges.R
@@ -60,8 +60,8 @@ bamregion2GRanges <- function(bamfile, bamindex=bamfile, region=NULL, pairedEndR
   }    
   
   #seqlevels(data) <- seqlevels(region)
-  #data <- keepSeqlevels(data, seqlevels(region), pruning.mode="coarse")
-  data <- keepSeqlevels(data, seqlevels(region))	
+  data <- keepSeqlevels(data, seqlevels(region), pruning.mode="coarse")
+  #data <- keepSeqlevels(data, seqlevels(region))	
   	
   return(data)
 }
